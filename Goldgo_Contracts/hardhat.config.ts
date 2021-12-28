cf40db7eec51df88f42c5e48b83b95df48732dfd
ECGgoldgo <72334833+ECGgoldgo@users.noreply.github.com>
2021-12-28 18:40:41 +0530

    Add files via upload

---
+++ Goldgo_Contracts/hardhat.config.ts
@@ -0,0 +1,33 @@
+import "@nomiclabs/hardhat-truffle5";
+import "@typechain/hardhat";
+import { HardhatUserConfig } from "hardhat/types";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  typechain: {
+    target: "truffle-v5",
+  },
+};
+
+export default config;
